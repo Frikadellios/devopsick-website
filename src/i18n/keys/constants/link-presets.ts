5b9c0834320a81168f2dfad62cfd3df5eb0f07fc
Hrihorii Ilin <devopsick@pm.me>
2024-05-05 14:08:21 +0000

    ++

---
+++ src/i18n/keys/constants/link-presets.ts
@@ -0,0 +1,18 @@
+import { LinkPreset, type NavBarLink } from '@/types/site.config'
+import I18nKey from '../I18nKeys'
+import { i18n } from '../translation'
+
+export const LinkPresets: { [key in LinkPreset]: NavBarLink } = {
+  [LinkPreset.Home]: {
+    name: i18n(I18nKey.home),
+    url: '/',
+  },
+  [LinkPreset.About]: {
+    name: i18n(I18nKey.about),
+    url: '/about/',
+  },
+  [LinkPreset.Archive]: {
+    name: i18n(I18nKey.archive),
+    url: '/archive/',
+  },
+}
