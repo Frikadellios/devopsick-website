879c6abf4e77be1fe901f0162113d8e9e8308ff7
Hrihorii Ilin <devopsick@pm.me>
2024-05-05 19:59:25 +0000

    +++

---
+++ src/typesafe-i18n/fr/index.ts
@@ -0,0 +1,8 @@
+import type { Translation } from '../i18n-types.js'
+
+const fr = {
+	// TODO: your translations go here
+	HI: 'Hallo {name}! Bitte hinterlasse einen Stern, wenn dir das Projekt gefällt: https://github.com/ivanhofer/typesafe-i18n',
+} satisfies Translation
+
+export default fr
